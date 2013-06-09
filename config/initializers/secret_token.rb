d798e927f87c4eaae5aa81d80ba4f842370f9e33
Jose Irizarry <jose@mrkp.cop>
2013-06-09 18:29:25 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Govreview::Application.config.secret_token = '9e99283b3dae115decdc7947d64d5afad80369a7b5a596dee64b83c54a77124f9a06926eed89516baaaa1ebbb1e712f860e1f91509a738f152f0c56ca6b41b8b'
